46dc5b2f4288241e48cfcb6fc6e33427fc834e28
kooldude410 <michaelhao410@gmail.com>
2022-04-06 22:04:20 -0700

    fixed endpoints

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["pickupitem", "levelup"]
+    const endpoints = ["/characters/pickupitem", "/characters/levelup"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
